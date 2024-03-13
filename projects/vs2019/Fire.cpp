518bfe44f0c02115743950da11b2d7ae21267b57
Alphenex <80922349+Alphenex@users.noreply.github.com>
2024-03-13 13:44:57 +0300

    Minor Changes

--- projects/vs2019/Fire.cpp
+++ projects/vs2019/Fire.cpp
@@ -30,8 +30,6 @@ void CFire::Precache()
 
 void CFire::Spawn()
 {
-	m_pSprite = nullptr; // Just in case, I don't want to deal with a headache
-
 	Precache();
 
 	pev->movetype = MOVETYPE_NONE;
@@ -45,6 +43,11 @@ void CFire::Spawn()
 		pev->ltime = gpGlobals->time + m_flLifeTime;
 	else
 		pev->ltime = -1; // Fire will basically never gonna die. 
+
+	if (m_pSprite)
+	{
+		m_pSprite->pev->frame = RANDOM_LONG(0, m_pSprite->Frames() - 1);
+	}
 
 	pev->nextthink = gpGlobals->time;
 }
@@ -70,7 +73,7 @@ void CFire::Think()
 {
 	if (m_pSprite && m_bActive)
 	{
-		Vector norg = pev->origin + Vector(0, 0, (pev->mins.z + pev->maxs.z) * 0.5);
+		Vector norg = pev->origin + Vector(0, 0, (pev->mins.z + pev->maxs.z - 4) * 0.5);
 		m_pSprite->pev->origin = norg;
 		m_pSprite->AnimateThink();
 	}
@@ -103,7 +106,7 @@ void CFire::KillFire()
 
 void CFire::CreateSprite()
 {
-	m_pSprite = CSprite::SpriteCreate(FIRE_SPRITE, pev->origin + Vector(0, 0, (pev->mins.z + pev->maxs.z) * 0.5), true);
+	m_pSprite = CSprite::SpriteCreate(FIRE_SPRITE, pev->origin + Vector(0, 0, (pev->mins.z + pev->maxs.z - 4) * 0.5), true);
 	m_pSprite->SetTransparency(kRenderTransAdd, 255, 255, 255, 255, kRenderFxNone);
 	m_pSprite->SetScale(1.0f);
 	m_pSprite->pev->framerate = 10.0f;
